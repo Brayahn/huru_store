c08fb8a9796e4301d5406761c5193e511d4e5c5d
Brayahn <brayahnmainah@gmail.com>
2022-08-31 21:07:11 +0300

    navbar - material ui spices

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import './Header.css';
-import huru from './huru.png'
+import huru from './huru.png';
+import SearchIcon from '@mui/icons-material/Search';
+import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
+
 
 function Header() {
   return (
@@ -11,23 +14,26 @@ function Header() {
 
     <div className='header_search'>  
     <input className='header_searchInput' type='text'/>
+    <SearchIcon className='header_searchIcon' />
     </div>
 
     <div className='header_nav'>
         <div className='header_option'>
             <span className='header_optionLineOne'> Hello </span>
-            <span className='header_optionLineOne'> Sign In  </span>
+            <span className='header_optionLineTwo'> Sign In  </span>
         </div>
 
         <div className='header_option'>
         <span className='header_optionLineOne'> Returns </span>
-            <span className='header_optionLineOne'>  Orders </span>
+            <span className='header_optionLineTwo'>  Orders </span>
         </div>
 
         <div className='header_option'>
         <span className='header_optionLineOne'> your  </span>
-            <span className='header_optionLineOne'> Prime </span>
+            <span className='header_optionLineTwo'> Prime </span>
         </div>
+
+        <div className='header_optionBasket'> </div>
 
     </div>
 
