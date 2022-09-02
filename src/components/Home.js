cd78da816eaffc03c8b8fc255c2cc3db033f6504
Brayahn <brayahnmainah@gmail.com>
2022-09-02 22:27:27 +0300

    added faded and official Logo resized

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,27 @@
 import React from 'react'
+import Backg from './back.jpg';
+import './Home.css';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div  className='home'>  
+        <div className='home__container'>
+        <img  className='home_image' src = {Backg} alt='Huru Store'/>
+
+        <div className='home_row'>
+
+        </div>
+
+        <div className='home_row'>
+            
+        </div>
+
+        <div className='home_row'>
+            
+        </div>
+
+        </div>
+    </div>
   )
 }
 
