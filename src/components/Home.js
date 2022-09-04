6299d169e1775f5f49c322b8db4cf6102fbbf6f2
Brayahn <brayahnmainah@gmail.com>
2022-09-04 04:53:36 +0300

    css fixes

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,28 +1,29 @@
-import React from 'react'
-import Backg from './back.jpg';
-import './Home.css';
+import React from "react";
+//import Backg from "./back.jpg";
+import "./Home.css";
+import Product from "./Product";
 
 const Home = () => {
   return (
-    <div  className='home'>  
-        <div className='home__container'>
-        <img  className='home_image' src = {Backg} alt='Huru Store'/>
-
-                <div className='home_row'>
-                        
-                </div>
-
-                <div className='home_row'>
-                    
-                </div>
-
-                <div className='home_row'>
-                    
-                </div>
-
+    <div className="home">
+      <div className="home__container">
+        <img className="home_image" src='https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/717OO5QwJnL._SX3000_.jpg' alt="Huru Store" />
+        {/*  https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/717OO5QwJnL._SX3000_.jpg */}
+        <div className="home_row">
+          <Product />
         </div>
-    </div>
-  )
-}
 
-export default Home
+        <div className="home_row">
+          <Product />
+        </div>
+
+        <div className="home_row">
+          <Product />
+          <Product />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,11 +1,27 @@
-import React from 'react';
-import './Product.css';
-
+import React from "react";
+import "./Product.css";
 
 const Product = () => {
   return (
-    <div>Product</div>
-  )
-}
+    <div className="product">
+      <div className="product_info">
+        <p>iPhone 13 Mini</p>
+        <p className="product_price">
+          <small>ksh</small>
+          <strong>89000</strong>
+        </p>
 
-export default Product
+        <div className="product_rating">
+          <p>⭐️</p>
+        </div>
+      </div>
+      <img
+        src="https://assets.swappie.com/cdn-cgi/image/width=600,height=600,fit=contain,format=auto/swappie-iphone-13-mini-red-back.png?v=5"
+        alt="iphone"
+      />
+      <button>Add to Cart</button>
+    </div>
+  );
+};
+
+export default Product;
