c8a56bbdb6c0cd36525ac5009e340a318b7bd33b
Brayahn <brayahnmainah@gmail.com>
2022-09-08 14:31:13 +0300

    routing implementation

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-import './App.css';
-import Header from './components/Header';
-import Home from './components/Home';
+import "./App.css";
+import Home from "./components/Home";
+import Header from "./components/Header";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
   return (
     //BEM
     <div className="app">
-      
-
-{/*Header */}
-  <Header/>
-
-{/* Home*/}
-<Home/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
