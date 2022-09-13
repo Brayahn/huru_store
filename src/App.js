94b5d024af7bb9fff7c65369d50ac4d4af075128
Brayahn <brayahnmainah@gmail.com>
2022-09-13 11:36:30 +0300

    navbar logo routing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import "./App.css";
 import Home from "./components/Home";
 import Header from "./components/Header";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Checkout from "./components/Checkout";
 
 function App() {
   return (
     //BEM
     <div className="app">
-      <Header />
+      {/*  <Header /> */}
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="cart" element={<Checkout />} />
+          {/*    <Route path="header" element={<Header />} />  */}
         </Routes>
       </BrowserRouter>
     </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,48 +1,47 @@
-import React from 'react'
-import './Header.css';
-import huru from './huru.png';
-import SearchIcon from '@mui/icons-material/Search';
-import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
-
+import React from "react";
+import "./Header.css";
+import huru from "./huru.png";
+import SearchIcon from "@mui/icons-material/Search";
+import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
-    <div className='header'>
+    <div className="header">
+  
+        <Link to="/">
+          <img className="header_logo" src={huru} alt="Huru Logo" />
+        </Link>
+      
 
-    <img className='header_logo' src={huru} alt="Huru Logo"/> 
+      <div className="header_search">
+        <input className="header_searchInput" type="text" />
+        <SearchIcon className="header_searchIcon" />
+      </div>
 
-
-    <div className='header_search'>  
-    <input className='header_searchInput' type='text'/>
-    <SearchIcon className='header_searchIcon' />
-    </div>
-
-    <div className='header_nav'>
-        <div className='header_option'>
-            <span className='header_optionLineOne'> Hello </span>
-            <span className='header_optionLineTwo'> Sign In  </span>
+      <div className="header_nav">
+        <div className="header_option">
+          <span className="header_optionLineOne"> Hello </span>
+          <span className="header_optionLineTwo"> Sign In </span>
         </div>
 
-        <div className='header_option'>
-        <span className='header_optionLineOne'> Returns </span>
-            <span className='header_optionLineTwo'>  Orders </span>
+        <div className="header_option">
+          <span className="header_optionLineOne"> Returns </span>
+          <span className="header_optionLineTwo"> Orders </span>
         </div>
 
-        <div className='header_option'>
-        <span className='header_optionLineOne'> your  </span>
-            <span className='header_optionLineTwo'> Prime </span>
+        <div className="header_option">
+          <span className="header_optionLineOne"> your </span>
+          <span className="header_optionLineTwo"> Prime </span>
         </div>
 
-        <div className='header_optionBasket'>
-            <ShoppingBasketIcon className='shopping_basket'/>
-            <span className='header_optionLineTwo header_basketCount'>0</span>
-         </div>
-
-
+        <div className="header_optionBasket">
+          <ShoppingBasketIcon className="shopping_basket" />
+          <span className="header_optionLineTwo header_basketCount">0</span>
+        </div>
+      </div>
     </div>
-
-    </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
