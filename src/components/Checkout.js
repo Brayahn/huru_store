9ad746fca0c84d3ef550616cff06ee19c3717202
Brayahn <brayahnmainah@gmail.com>
2022-09-17 22:13:36 +0300

    map items listing on checkout page - local served checkout products

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,9 +1,14 @@
+import { InsertEmoticon } from "@mui/icons-material";
 import React from "react";
 import "./Checkout.css";
+import CheckoutProduct from "./CheckoutProduct";
 import backg from "./checkout_.jpg";
+import { useStateValue } from "./StateProvider";
 import Subtotal from "./Subtotal";
 
 function Checkout() {
+  const [{ basket }, action] = useStateValue();
+
   return (
     <div className="checkout">
       <div className="checkout_left">
@@ -11,7 +16,15 @@ function Checkout() {
 
         <div>
           <h2 className="checkout_title"> Your Shopping Basket</h2>
-          {/* Cart Items*/}
+          {basket.map((item) => (
+            <CheckoutProduct
+              id={item.id}
+              title={item.title}
+              image={item.image}
+              price={item.price}
+              rating={item.rating}
+            />
+          ))}
         </div>
       </div>
       <div className="checkout_right">
