bf63f59288ccb5f0495704cf06e1ef1a4c66ab61
Brayahn <brayahnmainah@gmail.com>
2022-09-17 19:55:03 +0300

    onclick function- connect to reducer

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,10 +1,24 @@
 import React from "react";
 import "./Product.css";
+import { useStateValue } from "./StateProvider";
 
 const Product = ({ id, title, image, price, rating }) => {
+  const [basket, dispatch] = useStateValue();
+
+  console.log("this is the basket >>>", basket);
+
   const addToBasket = () => {
     //dispatch data to the data layer
-    
+    dispatch({  
+      type: "ADD_TO_BASKET",
+      item: {
+        id: id,
+        title: title,
+        image: image,
+        price: price,
+        rating: rating,
+      },
+    });
   };
   return (
     <div className="product">
@@ -23,7 +37,7 @@ const Product = ({ id, title, image, price, rating }) => {
             ))}
         </div>
       </div>
-      <img src={image} alt="image" />
+      <img src={image} alt="product" />
       <button onClick={addToBasket}>Add to Cart</button>
     </div>
   );
