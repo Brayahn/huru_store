ca5134840ab7229b02fa6fd02ee24b3d53b4e7fe
Brayahn <brayahnmainah@gmail.com>
2022-09-20 21:50:00 +0300

    login styled

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,16 @@
 import React from "react";
+import "./Login.css";
+import huru from "./huru.png";
+import { Link } from "react-router-dom";
 
 export default function Login() {
-  return <div>Login Page</div>;
+  return (
+    <div className="login">
+      <nav>
+        <Link to="/">
+          <img className="Login_Logo" src={huru} />
+        </Link>
+      </nav>
+    </div>
+  );
 }
