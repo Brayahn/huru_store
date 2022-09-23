fae5bf6a4477119bb57d52fc99e4fc410418c1f1
Brayahn <brayahnmainah@gmail.com>
2022-09-23 08:05:03 +0300

    styled precisely

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,16 +1,30 @@
-import React from "react";
-import "./Login.css";
-import huru from "./huru.png";
-import { Link } from "react-router-dom";
+    import React from "react";
+    import "./Login.css";
+    import huru from "./huru.png";
+    import { Link } from "react-router-dom";
 
-export default function Login() {
-  return (
-    <div className="login">
-      <nav>
-        <Link to="/">
-          <img className="Login_Logo" src={huru} />
-        </Link>
-      </nav>
-    </div>
-  );
-}
+    export default function Login() {
+    return (
+        <div className="login">
+        <nav>
+            <Link to="/">
+            <img className="Login_Logo" src={huru} />
+            </Link>
+        </nav>
+
+        <div className="Login_Container">
+            <h1> Sign In</h1>
+            <form>
+            <h5>Email</h5>
+            <input type="email" />
+            <h5>Password</h5>
+            <input type="password" />
+            <button className="Login_SignIn_Button">Sign In</button>
+            </form><p>
+            By signing in you agree to Huru's Conditions of Use and Sale. Please see
+            our Privacy Notice, our Cookies Notice and our Interest-Based Ads</p>
+            <button className="Login_RegisterButton">Create a Huru Account</button>
+        </div>
+        </div>
+    );
+    }
