1fb4e3a122cd634c1a6bfcfdfc838c7c1dc5e409
Brayahn <brayahnmainah@gmail.com>
2022-10-16 11:29:33 +0300

    sign out using firebase auth function

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,9 +5,16 @@ import SearchIcon from "@mui/icons-material/Search";
 import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
 import { Link } from "react-router-dom";
 import { useStateValue } from "./StateProvider";
+import { auth } from "../firebase";
 
 function Header() {
-  const [{ basket }, dispatch] = useStateValue();
+  const [{ basket, user }, dispatch] = useStateValue();
+
+  const handleAuthentication = () => {
+    if (user) {
+      auth.signOut();
+    }
+  };
 
   return (
     <div className="header">
@@ -24,10 +31,12 @@ function Header() {
 
       <div className="header_nav">
         <nav>
-          <Link to="/login">
-            <div className="header_option">
-              <span className="header_optionLineOne"> Hello </span>
-              <span className="header_optionLineTwo"> Sign In </span>
+          <Link to={!user && "/login"}>
+            <div onClick={handleAuthentication} className="header_option">
+              <span className="header_optionLineOne"> Hello Stranger</span>
+              <span className="header_optionLineTwo">
+                {user ? "Sign Out" : "Sign In"}
+              </span>
             </div>
           </Link>
         </nav>
